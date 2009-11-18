eda47b61b5b43c5a4173eff4a13888758de08927
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2009-11-18 17:01:09 +0000

    Fixed bug introduced by utf-8 patch with fix provided by Henry Ludemann. All unit tests are now passing.

--- src/lib_json/json_writer.cpp
+++ src/lib_json/json_writer.cpp
@@ -120,44 +120,52 @@ std::string valueToQuotedString( const char *value )
    std::string result;
    result.reserve(maxsize); // to avoid lots of mallocs
    result += "\"";
-   for (const char* c=value; *c != 0; ++c){
-      switch(*c){
+   for (const char* c=value; *c != 0; ++c)
+   {
+      switch(*c)
+      {
          case '\"':
-	 result += "\\\"";
-	 break;
-	 case '\\':
-	 result += "\\\\";
-	 break;
-	 case '\b':
-	 result += "\\b";
-	 break;
-	 case '\f':
-	 result += "\\f";
-	 break;
-	 case '\n':
-	 result += "\\n";
-	 break;
-	 case '\r':
-	 result += "\\r";
-	 break;
-	 case '\t':
-	 result += "\\t";
-	 break;
-	 case '/':
-	 // Even though \/ is considered a legal escape in JSON, a bare
-	 // slash is also legal, so I see no reason to escape it.
-	 // (I hope I am not misunderstanding something.)
-	 default:
-	    if ( isControlCharacter( *c ) )
-       {
-          std::ostringstream oss;
-          oss << "\\u" << std::hex << std::uppercase << std::setfill('0') << std::setw(4) << static_cast<int>(*c);
-          result += oss.str();
-       }
-       else
-         result += *c;
+            result += "\\\"";
+            break;
+         case '\\':
+            result += "\\\\";
+            break;
+         case '\b':
+            result += "\\b";
+            break;
+         case '\f':
+            result += "\\f";
+            break;
+         case '\n':
+            result += "\\n";
+            break;
+         case '\r':
+            result += "\\r";
+            break;
+         case '\t':
+            result += "\\t";
+            break;
+         //case '/':
+            // Even though \/ is considered a legal escape in JSON, a bare
+            // slash is also legal, so I see no reason to escape it.
+            // (I hope I am not misunderstanding something.
+            // blep notes: actually escaping \/ may be useful in javascript to avoid </ 
+            // sequence.
+            // Should add a flag to allow this compatibility mode and prevent this 
+            // sequence from occurring.
+         default:
+            if ( isControlCharacter( *c ) )
+            {
+               std::ostringstream oss;
+               oss << "\\u" << std::hex << std::uppercase << std::setfill('0') << std::setw(4) << static_cast<int>(*c);
+               result += oss.str();
+            }
+            else
+            {
+               result += *c;
+            }
+            break;
       }
-      break;
    }
    result += "\"";
    return result;
