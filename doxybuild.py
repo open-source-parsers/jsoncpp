130730ffd795013923e7b1afa17313d8a11eb909
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2010-03-13 11:14:49 +0000

    Added NEWS.txt that provides a synopsis of the change since the last version. Integrated NEWS.txt in documentation.

--- doxybuild.py
+++ doxybuild.py
@@ -127,6 +127,7 @@ def build_doc( options,  make_release=False ):
         tarball_sources = [
             output_dir,
             'README.txt',
+            'NEWS.txt',
             'version'
             ]
         tarball_basedir = os.path.join( output_dir, html_output_dirname )
