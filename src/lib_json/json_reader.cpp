09439b7bc79eced6eb072695a9a34fe3abd3b231
Christopher Dunn <cdunn2001@gmail.com>
2014-04-19 21:19:24 +0000

    Comment reading/write improvements
    This patch fixes some aspects of reading and writing comments:
    - Multiple C++-style comments before a Json value had extra newlines appended to them. This patch removes the addition of those newlines.
    - Comments written before Json values in the StyledWriter were not indented to match the indentation level of the value. This patch adds indentation to comments.
    - Fixed inconsistency in newlines following C- and C++-style comments being saved as part of the comment. All newlines at the end of a comment are now removed.
    - Added an additional test of comments.
    
    https://sourceforge.net/p/jsoncpp/patches/25/

--- src/lib_json/json_reader.cpp
+++ src/lib_json/json_reader.cpp
@@ -191,6 +191,17 @@ Reader::readValue()
 
    if ( collectComments_  &&  !commentsBefore_.empty() )
    {
+      // Remove newline characters at the end of the comments
+      size_t lastNonNewline = commentsBefore_.find_last_not_of("\r\n");
+      if (lastNonNewline != std::string::npos)
+      {
+         commentsBefore_.erase(lastNonNewline+1);
+      }
+      else
+      {
+         commentsBefore_.clear();
+      }
+
       currentValue().setComment( commentsBefore_, commentBefore );
       commentsBefore_ = "";
    }
--- src/lib_json/json_writer.cpp
+++ src/lib_json/json_writer.cpp
@@ -483,7 +483,20 @@ StyledWriter::writeCommentBeforeValue( const Value &root )
 {
    if ( !root.hasComment( commentBefore ) )
       return;
-   document_ += normalizeEOL( root.getComment( commentBefore ) );
+
+   document_ += "\n";
+   writeIndent();
+   std::string normalizedComment = normalizeEOL( root.getComment( commentBefore ) );
+   std::string::const_iterator iter = normalizedComment.begin();
+   while ( iter != normalizedComment.end() )
+   {
+      document_ += *iter;
+      if ( *iter == '\n' && *(iter+1) == '/' )
+         writeIndent();
+      ++iter;
+   }
+
+   // Comments are stripped of newlines, so add one here
    document_ += "\n";
 }
 
