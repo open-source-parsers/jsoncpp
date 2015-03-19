c083835261ebbfd2f599a93fd62ac3235a0e78b0
selaselah <selah@outlook.com>
2015-03-19 19:18:58 +0800

    fix find_program() bug: no result in not-win sys

--- doxybuild.py
+++ doxybuild.py
@@ -31,7 +31,7 @@ def find_program(*filenames):
     paths = os.environ.get('PATH', '').split(os.pathsep)
     suffixes = ('win32' in sys.platform) and '.exe .com .bat .cmd' or ''
     for filename in filenames:
-        for name in [filename+ext for ext in suffixes.split()]:
+        for name in [filename+ext for ext in suffixes.split(' ')]:
             for directory in paths:
                 full_path = os.path.join(directory, name)
                 if os.path.isfile(full_path):
