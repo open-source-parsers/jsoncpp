4a61f4fb54d98e452c380a882eb736bac5bdad80
Tengiz Sharafiev <btolfa@gmail.com>
2015-03-25 17:09:03 +0300

    remove Json::Value::operator!()
    operator! is no longer necessary because of explicit operator bool()

--- include/json/value.h
+++ include/json/value.h
@@ -344,9 +344,6 @@ Json::Value obj_value(Json::objectValue); // {}
   /// otherwise, false.
   bool empty() const;
 
-  /// Return isNull()
-  bool operator!() const;
-
   /** \brief Return ! isNull();
    *
    * Example of usage:
