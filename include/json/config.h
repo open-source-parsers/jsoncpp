0035324e17ce1e21288a3fb125ede142406baf5a
Motti <lanzkron@gmail.com>
2015-04-27 14:54:32 +0300

    Revert parts of "Initial code for move constructor - not compiled or tested"
    This reverts parts of commit df0dc466ec0439e21b16b104a46f4622e21eeeb6.

--- include/json/config.h
+++ include/json/config.h
@@ -67,40 +67,21 @@
 // Storages, and 64 bits integer support is disabled.
 // #define JSON_NO_INT64 1
 
-#if defined(_MSC_VER) // MSVC
-	#if _MSC_VER <= 1200 // MSVC 6
-		// Microsoft Visual Studio 6 only support conversion from __int64 to double
-		// (no conversion from unsigned __int64).
-		#define JSON_USE_INT64_DOUBLE_CONVERSION 1
-		// Disable warning 4786 for VS6 caused by STL (identifier was truncated to '255'
-		// characters in the debug information)
-		// All projects I've ever seen with VS6 were using this globally (not bothering
-		// with pragma push/pop).
-		#pragma warning(disable : 4786)
-	#endif // MSVC 6
+#if defined(_MSC_VER) && _MSC_VER <= 1200 // MSVC 6
+// Microsoft Visual Studio 6 only support conversion from __int64 to double
+// (no conversion from unsigned __int64).
+#define JSON_USE_INT64_DOUBLE_CONVERSION 1
+// Disable warning 4786 for VS6 caused by STL (identifier was truncated to '255'
+// characters in the debug information)
+// All projects I've ever seen with VS6 were using this globally (not bothering
+// with pragma push/pop).
+#pragma warning(disable : 4786)
+#endif // if defined(_MSC_VER)  &&  _MSC_VER < 1200 // MSVC 6
 
-	#if _MSC_VER >= 1500 // MSVC 2008
-		/// Indicates that the following function is deprecated.
-		#define JSONCPP_DEPRECATED(message) __declspec(deprecated(message))
-	#endif
-
-	#if _MSC_VER >= 1600 // MSVC 2010
-		#define JSON_HAS_RVALUE_REFERENCES
-	#endif 
-#endif // defined(_MSC_VER)
-
-
-#ifdef __clang__
-	#if __has_feature(cxx_rvalue_references)
-		#define JSON_HAS_RVALUE_REFERENCES
-	#endif 
-#elif defined __GNUC__
-	#if defined(__GXX_EXPERIMENTAL_CXX0X__) 
-		#define JSON_HAS_RVALUE_REFERENCES
-	#endif
-#endif // __clang__ || __GNUC__
-
-
+#if defined(_MSC_VER) && _MSC_VER >= 1500 // MSVC 2008
+/// Indicates that the following function is deprecated.
+#define JSONCPP_DEPRECATED(message) __declspec(deprecated(message))
+#endif
 
 #if !defined(JSONCPP_DEPRECATED)
 #define JSONCPP_DEPRECATED(message)
