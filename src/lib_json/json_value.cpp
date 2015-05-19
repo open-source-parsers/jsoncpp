open-source-parsers/jsoncpp

commit 48d9a92a1b4621e69533af543c1f0611abc47ddc

do intermediate step in order to omit "dereferencing type-punned pointer" error

src/lib_json/json_value.cpp
@@ -34,7 +34,8 @@ namespace Json {
 #define ALIGNAS(byte_alignment)
 #endif
 static const unsigned char ALIGNAS(8) kNull[sizeof(Value)] = {0};
-const Value& Value::null = reinterpret_cast<const Value&>(kNull);
+const unsigned char& kNullRef = kNull[0];
+const Value& Value::null = reinterpret_cast<const Value&>(kNullRef);
 
 const Int Value::minInt = Int(~(UInt(-1) / 2));
 const Int Value::maxInt = Int(UInt(-1) / 2);

commit c49a83d1fddf6250a1be2bd12eaed999f3d3abdc

Ensure Json::Value::null{,Ref} are initialized at compile time.
This bug was introduced in commit 48d9a92a1b4621e69533af543c1f0611abc47ddc.

gcc now puts Json::Value::null{,Ref} into bss section:

  $ nm libjsoncpp.a | c++filt | grep Value::null
  0000000000000000 B Json::Value::null
  0000000000000008 B Json::Value::nullRef

When we access them in constructor of global objects, since the order of
initialization is unpredictable, we may dereference a null pointer.

After applying this patch, gcc puts them into rodata section:

  $ nm libjsoncpp.a | c++filt | grep Value::null
  0000000000000008 R Json::Value::null
  0000000000000010 R Json::Value::nullRef

src/lib_json/json_value.cpp
@@ -32,9 +32,8 @@ namespace Json {
 #define ALIGNAS(byte_alignment)
 #endif
 static const unsigned char ALIGNAS(8) kNull[sizeof(Value)] = { 0 };
-const unsigned char& kNullRef = kNull[0];
-const Value& Value::null = reinterpret_cast<const Value&>(kNullRef);
-const Value& Value::nullRef = null;
+const Value& Value::null = reinterpret_cast<const Value&>(kNull[0]);
+const Value& Value::nullRef = reinterpret_cast<const Value&>(kNull[0]);
 
 const Int Value::minInt = Int(~(UInt(-1) / 2));
 const Int Value::maxInt = Int(UInt(-1) / 2);