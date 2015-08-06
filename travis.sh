aa6335908d98a6cce8c22b04ad921c90ce9e32f9
Robert Dailey <rcdailey@gmail.com>
2015-08-06 10:17:09 -0500

    Use a consistent target name for jsoncpp in CMake scripts
    The target name for the jsoncpp library would change based on whether
    we were building a static or shared library. This inconsistency made
    it difficult and unintuitive to pull in jsoncpp as a submodule in
    another repository and link to it directly via other CMake scripts.
    
    Having a consistent target name will allow libraries with their own
    CMake scripts to reliably refer to jsoncpp as a dependency.
    
    Other Changes:
    
    * `BUILD_SHARED_LIBS` and `BUILD_STATIC_LIBS` removed in favor of
      `JSONCPP_LIBRARY_TYPE`, which allows you to pick either `SHARED`
      or `STATIC` library variations. This change was made to prevent
      both shared and static libraries being built at the same time.
      This isn't allowed anymore since we only generate 1 target for the
      jsoncpp library now.
    * Clean up closing statements for if conditions, functions, macros,
      and other entities. Newer versions of CMake do not require you to
      redundantly respecify the parameters to the opening arguments.
    * `travis.sh` build script updated to perform CMake generation in an
      out-of-source binary directory. This will prevent the temporary
      generated output files from intermixing into the source tree and
      allow for multiple generations with different configurations using
      the same source tree.

--- travis.sh
+++ travis.sh
@@ -17,13 +17,38 @@ set -vex
 
 env | sort
 
-cmake -DJSONCPP_WITH_CMAKE_PACKAGE=$CMAKE_PKG -DBUILD_SHARED_LIBS=$SHARED_LIB -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_VERBOSE_MAKEFILE=$VERBOSE_MAKE .
-make
+# $1 = Binary directory name
+# $2 = Value for JSONCPP_LIBRARY_TYPE (SHARED or STATIC)
+cmakebuild()
+{
+    if [ ! -d "$1" ]; then
+        mkdir "$1"
+    fi
 
-# Python is not available in Travis for osx.
-#  https://github.com/travis-ci/travis-ci/issues/2320
-if [ "$TRAVIS_OS_NAME" != "osx" ]
-then
-  make jsoncpp_check
-  valgrind --error-exitcode=42 --leak-check=full ./src/test_lib_json/jsoncpp_test
+    cd "$1"
+
+    cmake .. -G "Unix Makefiles" \
+        -DJSONCPP_WITH_CMAKE_PACKAGE="$CMAKE_PKG" \
+        -DJSONCPP_LIBRARY_TYPE="$2" \
+        -DCMAKE_BUILD_TYPE="$BUILD_TYPE" \
+        -DCMAKE_VERBOSE_MAKEFILE="$VERBOSE_MAKE"
+
+    make
+
+    # Python is not available in Travis for osx.
+    #  https://github.com/travis-ci/travis-ci/issues/2320
+    if [ "$TRAVIS_OS_NAME" != "osx" ]; then
+        make jsoncpp_check
+        valgrind --error-exitcode=42 --leak-check=full ./src/test_lib_json/jsoncpp_test
+    fi
+
+    cd -
+}
+
+if [ "$SHARED_LIB" = "ON" ]; then
+    cmakebuild build_shared SHARED
+fi
+
+if [ "$STATIC_LIB" = "ON" ]; then
+    cmakebuild build_static STATIC
 fi
