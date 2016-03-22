1c47796479b4d49ac0ebd2f57528a0476a36804b
Christopher Dunn <cdunn2001@gmail.com>
2016-03-21 20:44:03 -0500

    JSONCPP_USING_SECURE_MEMORY default is 0
    Re: #410

--- include/json/version.h
+++ include/json/version.h
@@ -13,7 +13,7 @@
 #ifdef JSONCPP_USING_SECURE_MEMORY
 #undef JSONCPP_USING_SECURE_MEMORY
 #endif
-#define JSONCPP_USING_SECURE_MEMORY 1
+#define JSONCPP_USING_SECURE_MEMORY 0
 // If non-zero, the library zeroes any memory that it has allocated before
 // it frees its memory.
 
