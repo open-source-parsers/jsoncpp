b9afdf190dbccbf3a59a3aba86eb295c0fa07068
Christopher Dunn <cdunn2001@gmail.com>
2016-08-21 19:58:43 -0500

    Use int64_t for 64bit ints
    resolves #509

--- include/json/config.h
+++ include/json/config.h
@@ -152,8 +152,8 @@ typedef unsigned int LargestUInt;
 typedef __int64 Int64;
 typedef unsigned __int64 UInt64;
 #else                 // if defined(_MSC_VER) // Other platforms, use long long
-typedef long long int Int64;
-typedef unsigned long long int UInt64;
+typedef int64_t Int64;
+typedef uint64_t UInt64;
 #endif // if defined(_MSC_VER)
 typedef Int64 LargestInt;
 typedef UInt64 LargestUInt;
