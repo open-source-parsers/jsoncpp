7bcc335d01404ea0b81b52625a35db5a21d7d857
Frank Richter <frank.richter@gmail.com>
2019-03-22 10:15:08 +0100

    Explicitly set JSON_API to 'default' visibility on clang & gcc

--- include/json/config.h
+++ include/json/config.h
@@ -48,6 +48,8 @@
 #if defined(_MSC_VER) || defined(__MINGW32__)
 #define JSON_API __declspec(dllexport)
 #define JSONCPP_DISABLE_DLL_INTERFACE_WARNING
+#elif defined(__GNUC__) || defined(__clang__)
+#define JSON_API __attribute__((visibility("default")))
 #endif // if defined(_MSC_VER)
 #elif defined(JSON_DLL)
 #if defined(_MSC_VER) || defined(__MINGW32__)
