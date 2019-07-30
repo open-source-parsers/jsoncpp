d3121d73fc6e429aec96d69ed95492ef85642c8a
Google AutoFuzz Team <security-tps@google.com>
2019-07-30 15:47:46 -0400

    Get rid of unnecessary comments

--- src/test_lib_json/fuzz.cpp
+++ src/test_lib_json/fuzz.cpp
@@ -14,13 +14,6 @@
 #include "json/value.h"
 #include "json/writer.h"
 
-// #include <cstdint>
-// #include <json/config.h>
-// #include <json/json.h>
-// #include <memory>
-// #include <stdint.h>
-// #include <string>
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   std::string json_string(reinterpret_cast<const char *>(data), size);
   Json::Reader reader(Json::Features::strictMode());
@@ -40,39 +33,3 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   styled_stream_writer.write(sstream, value);
   return 0;
 }
-
-// namespace Json {
-// class Exception;
-// }
-
-// extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-//   Json::CharReaderBuilder builder;
-
-//   if (size < sizeof(uint32_t)) {
-//     return 0;
-//   }
-
-//   uint32_t hash_settings = *(const uint32_t*)data;
-//   data += sizeof(uint32_t);
-
-//   builder.settings_["failIfExtra"] = hash_settings & (1 << 0);
-//   builder.settings_["allowComments_"] = hash_settings & (1 << 1);
-//   builder.settings_["strictRoot_"] = hash_settings & (1 << 2);
-//   builder.settings_["allowDroppedNullPlaceholders_"] = hash_settings & (1 << 3);
-//   builder.settings_["allowNumericKeys_"] = hash_settings & (1 << 4);
-//   builder.settings_["allowSingleQuotes_"] = hash_settings & (1 << 5);
-//   builder.settings_["failIfExtra_"] = hash_settings & (1 << 6);
-//   builder.settings_["rejectDupKeys_"] = hash_settings & (1 << 7);
-//   builder.settings_["allowSpecialFloats_"] = hash_settings & (1 << 8);
-
-//   std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
-
-//   Json::Value root;
-//   const char* data_str = reinterpret_cast<const char*>(data);
-//   try {
-//     reader->parse(data_str, data_str + size, &root, nullptr);
-//   } catch (Json::Exception const&) {
-//   }
-//   // Whether it succeeded or not doesn't matter.
-//   return 0;
-// }
