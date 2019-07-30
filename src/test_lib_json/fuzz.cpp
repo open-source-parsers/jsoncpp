f7edafe8487757587def1ff3bdc4a498beedca8b
Google AutoFuzz Team <security-tps@google.com>
2019-07-30 14:45:31 -0400

    Modify fuzzer for OSS-Fuzz

--- src/test_lib_json/fuzz.cpp
+++ src/test_lib_json/fuzz.cpp
@@ -5,45 +5,74 @@
 
 #include "fuzz.h"
 
-#include <cstdint>
-#include <json/config.h>
-#include <json/json.h>
-#include <memory>
-#include <stdint.h>
+#include <sstream>
 #include <string>
 
-namespace Json {
-class Exception;
-}
+#include "jsoncpp/config.h"
+#include "jsoncpp/features.h"
+#include "jsoncpp/reader.h"
+#include "jsoncpp/value.h"
+#include "jsoncpp/writer.h"
 
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  Json::CharReaderBuilder builder;
+// #include <cstdint>
+// #include <json/config.h>
+// #include <json/json.h>
+// #include <memory>
+// #include <stdint.h>
+// #include <string>
 
-  if (size < sizeof(uint32_t)) {
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+  std::string json_string(reinterpret_cast<const char *>(data), size);
+  Json::Reader reader(Json::Features::strictMode());
+  Json::Value value;
+  const bool success = reader.parse(json_string, value, false);
+  if (!success) {
     return 0;
   }
 
-  uint32_t hash_settings = *(const uint32_t*)data;
-  data += sizeof(uint32_t);
+  // Write with StyledWriter
+  Json::StyledWriter styled_writer;
+  styled_writer.write(value);
 
-  builder.settings_["failIfExtra"] = hash_settings & (1 << 0);
-  builder.settings_["allowComments_"] = hash_settings & (1 << 1);
-  builder.settings_["strictRoot_"] = hash_settings & (1 << 2);
-  builder.settings_["allowDroppedNullPlaceholders_"] = hash_settings & (1 << 3);
-  builder.settings_["allowNumericKeys_"] = hash_settings & (1 << 4);
-  builder.settings_["allowSingleQuotes_"] = hash_settings & (1 << 5);
-  builder.settings_["failIfExtra_"] = hash_settings & (1 << 6);
-  builder.settings_["rejectDupKeys_"] = hash_settings & (1 << 7);
-  builder.settings_["allowSpecialFloats_"] = hash_settings & (1 << 8);
-
-  std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
-
-  Json::Value root;
-  const char* data_str = reinterpret_cast<const char*>(data);
-  try {
-    reader->parse(data_str, data_str + size, &root, nullptr);
-  } catch (Json::Exception const&) {
-  }
-  // Whether it succeeded or not doesn't matter.
+  // Write with StyledStreamWriter
+  Json::StyledStreamWriter styled_stream_writer;
+  JSONCPP_OSTRINGSTREAM sstream;
+  styled_stream_writer.write(sstream, value);
   return 0;
 }
+
+// namespace Json {
+// class Exception;
+// }
+
+// extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+//   Json::CharReaderBuilder builder;
+
+//   if (size < sizeof(uint32_t)) {
+//     return 0;
+//   }
+
+//   uint32_t hash_settings = *(const uint32_t*)data;
+//   data += sizeof(uint32_t);
+
+//   builder.settings_["failIfExtra"] = hash_settings & (1 << 0);
+//   builder.settings_["allowComments_"] = hash_settings & (1 << 1);
+//   builder.settings_["strictRoot_"] = hash_settings & (1 << 2);
+//   builder.settings_["allowDroppedNullPlaceholders_"] = hash_settings & (1 << 3);
+//   builder.settings_["allowNumericKeys_"] = hash_settings & (1 << 4);
+//   builder.settings_["allowSingleQuotes_"] = hash_settings & (1 << 5);
+//   builder.settings_["failIfExtra_"] = hash_settings & (1 << 6);
+//   builder.settings_["rejectDupKeys_"] = hash_settings & (1 << 7);
+//   builder.settings_["allowSpecialFloats_"] = hash_settings & (1 << 8);
+
+//   std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+
+//   Json::Value root;
+//   const char* data_str = reinterpret_cast<const char*>(data);
+//   try {
+//     reader->parse(data_str, data_str + size, &root, nullptr);
+//   } catch (Json::Exception const&) {
+//   }
+//   // Whether it succeeded or not doesn't matter.
+//   return 0;
+// }
