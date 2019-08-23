00be3b2005b777db110930fb5031122994e805cd
chenguoping <chenguopingdota@163.com>
2019-08-23 15:46:51 +0800

    reinforce readToken function and add simple tests

--- src/lib_json/json_reader.cpp
+++ src/lib_json/json_reader.cpp
@@ -1230,6 +1230,14 @@ bool OurReader::readToken(Token& token) {
       ok = features_.allowSpecialFloats_ && match("nfinity", 7);
     }
     break;
+  case '+':
+    if (readNumber(true)) {
+      token.type_ = tokenNumber;
+    } else {
+      token.type_ = tokenPosInf;
+      ok = features_.allowSpecialFloats_ && match("nfinity", 7);
+    }
+    break;
   case 't':
     token.type_ = tokenTrue;
     ok = match("rue", 3);
