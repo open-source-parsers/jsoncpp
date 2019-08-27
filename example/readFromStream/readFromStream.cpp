e87b86cbe1b8df07e2c2827a487df8460496f261
dota17 <50514813+dota17@users.noreply.github.com>
2019-08-27 16:52:38 +0800

    Update readFromStream.cpp

--- example/readFromStream/readFromStream.cpp
+++ example/readFromStream/readFromStream.cpp
@@ -15,18 +15,18 @@ $./readFromStream
 // comment tail
 */
 int main(int argc, char* argv[]) {
-  Json::Value jsonRoot;
+  Json::Value root;
   std::ifstream ifs;
   ifs.open(argv[1]);
-  jsonRoot.clear();
+  root.clear();
 
   Json::CharReaderBuilder builder;
   builder["collectComments"] = true;
   JSONCPP_STRING errs;
-  if (!parseFromStream(builder, ifs, &jsonRoot, &errs)) {
+  if (!parseFromStream(builder, ifs, &root, &errs)) {
     std::cout << errs << std::endl;
     return EXIT_FAILURE;
   }
-  std::cout << jsonRoot << std::endl;
+  std::cout << root << std::endl;
   return EXIT_SUCCESS;
 }
