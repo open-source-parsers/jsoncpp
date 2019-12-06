c82c4cd1922f424f8f64d259a2b2f5dbdb2a76dd
Billy Donahue <BillyDonahue@users.noreply.github.com>
2019-12-06 13:53:55 -0500

    Update main.cpp

--- src/test_lib_json/main.cpp
+++ src/test_lib_json/main.cpp
@@ -124,7 +124,7 @@ Json::String ValueTest::normalizeFloatingPointStr(const Json::String& s) {
   auto index = s.find_last_of("eE");
   if (index == s.npos)
     return s;
-  std::size_t signWidth = (s[index + 1] == '+' || s[index + 1] == '-' ) ? 1 : 0;
+  std::size_t signWidth = (s[index + 1] == '+' || s[index + 1] == '-') ? 1 : 0;
   auto exponentStartIndex = index + 1 + signWidth;
   Json::String normalized = s.substr(0, exponentStartIndex);
   auto indexDigit = s.find_first_not_of('0', exponentStartIndex);
