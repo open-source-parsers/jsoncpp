ded7d84ce5925f18a0f755ffee2f165ba5101d59
lizz <lizz@sensetime.com>
2020-04-10 23:42:04 +0800

    fix
    Signed-off-by: lizz <lizz@sensetime.com>

--- src/lib_json/json_writer.cpp
+++ src/lib_json/json_writer.cpp
@@ -990,7 +990,8 @@ void BuiltStyledStreamWriter::writeValue(Value const& value) {
         writeCommentBeforeValue(childValue);
         writeWithIndent(valueToQuotedStringN(
             name.data(), static_cast<unsigned>(name.length()), emitUTF8_));
-        if ((childValue.type() == objectValue) ||
+        if ((childValue.type() == objectValue &&
+             !value.getMemberNames().empty()) ||
             (childValue.type() == arrayValue && childValue.size() > 0 &&
              ((cs_ == CommentStyle::All) || isMultilineArray(childValue))))
           *sout_ << colonSymbolNoTrailingSpace_;
