97925263f53f6d168b56dbee188dcef4abba06eb
gaozilai <utrust@163.com>
2020-10-12 16:31:28 +0800

    Add example code of Visual Studio 2017 on windows OS.

---
+++ example/vs2017/jsonTest/jsonTest/framework.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN            // 从 Windows 头中排除极少使用的资料
+#endif
+
+#include "targetver.h"
+
+#define _ATL_CSTRING_EXPLICIT_CONSTRUCTORS      // 某些 CString 构造函数将是显式的
+
+// 关闭 MFC 的一些常见且经常可放心忽略的隐藏警告消息
+#define _AFX_ALL_WARNINGS
+
+#include <afxwin.h>         // MFC 核心组件和标准组件
+#include <afxext.h>         // MFC 扩展
+
+
+
+
+
+#ifndef _AFX_NO_OLE_SUPPORT
+#include <afxdtctl.h>           // MFC 对 Internet Explorer 4 公共控件的支持
+#endif
+#ifndef _AFX_NO_AFXCMN_SUPPORT
+#include <afxcmn.h>             // MFC 对 Windows 公共控件的支持
+#endif // _AFX_NO_AFXCMN_SUPPORT
+
+#include <afxcontrolbars.h>     // MFC 支持功能区和控制条
+
+
+
+
+
+
+
+
+
+
+
---
+++ example/vs2017/jsonTest/jsonTest/jsonTestDlg.h
@@ -0,0 +1,38 @@
+﻿
+// jsonTestDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CjsonTestDlg 对话框
+class CjsonTestDlg : public CDialogEx
+{
+// 构造
+public:
+	CjsonTestDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_JSONTEST_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+    afx_msg void OnBnClickedreadfromstream();
+	DECLARE_MESSAGE_MAP()
+public:
+    afx_msg void OnBnClickedreadfromstring();
+    afx_msg void OnBnClickedstreamwrite();
+    afx_msg void OnBnClickedstringwrite();
+};
