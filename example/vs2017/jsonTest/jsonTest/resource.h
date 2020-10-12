a895bb25acc9f58f3583fa93645d3414bc18c64f
gaozilai <utrust@163.com>
2020-10-12 16:31:28 +0800

    Add example code of Visual Studio 2017 on windows OS.

---
+++ example/vs2017/jsonTest/jsonTest/resource.h
@@ -0,0 +1,22 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 jsonTest.rc 使用
+//
+#define IDD_JSONTEST_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDC_readFromStream              1000
+#define IDC_readFromString              1001
+#define IDC_streamWrite                 1002
+#define IDC_streamWrite2                1003
+#define IDC_stringWrite                 1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
