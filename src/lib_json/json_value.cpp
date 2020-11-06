dfe281e00ce2a172ddc2571d329b8ddb7706d738
Jordan Bayles <jophba@chromium.org>
2020-11-06 11:58:25 -0800

    Fixup/clang-format

--- src/lib_json/json_value.cpp
+++ src/lib_json/json_value.cpp
@@ -1373,7 +1373,8 @@ bool Value::isObject() const { return type() == objectValue; }
 Value::Comments::Comments(const Comments& that)
     : ptr_{cloneUnique(that.ptr_)} {}
 
-Value::Comments::Comments(Comments&& that) noexcept : ptr_{std::move(that.ptr_)} {}
+Value::Comments::Comments(Comments&& that) noexcept
+    : ptr_{std::move(that.ptr_)} {}
 
 Value::Comments& Value::Comments::operator=(const Comments& that) {
   ptr_ = cloneUnique(that.ptr_);
