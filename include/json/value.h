fe9663e7edaf5c179af9c42ffd47428b83f8842a
Billy Donahue <billy.donahue@gmail.com>
2020-12-21 04:41:37 -0500

    `Json::ValueIterator` operators `*` and `->` need to be const
    Fixes #1249.

--- include/json/value.h
+++ include/json/value.h
@@ -918,8 +918,8 @@ public:
    *  because the returned references/pointers can be used
    *  to change state of the base class.
    */
-  reference operator*() { return deref(); }
-  pointer operator->() { return &deref(); }
+  reference operator*() const { return const_cast<reference>(deref()); }
+  pointer operator->() const { return const_cast<pointer>(&deref()); }
 };
 
 inline void swap(Value& a, Value& b) { a.swap(b); }
