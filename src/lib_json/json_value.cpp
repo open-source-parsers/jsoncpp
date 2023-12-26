bbef9e336412496bd3f01176fdc3dddfd126c7c9
Timur Digitaev <timurdigitaev@MacBook-Air-Timur.local>
2023-12-26 14:14:39 +0300

    added_check_for_nullptr

--- src/lib_json/json_value.cpp
+++ src/lib_json/json_value.cpp
@@ -24,7 +24,11 @@ static int msvc_pre1900_c99_vsnprintf(char* outBuf, size_t size,
                                       const char* format, va_list ap) {
   int count = -1;
   if (size != 0)
-    count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+    {
+      if (!outBuf)
+        return 0;
+      count = _vsnprintf_s(outBuf, size, _TRUNCATE, format, ap);
+    }
   if (count == -1)
     count = _vscprintf(format, ap);
   return count;
