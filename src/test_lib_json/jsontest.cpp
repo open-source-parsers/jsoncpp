719bec0a6ca2fabf144d63ee6c08b22e0f0803e1
parsival <arturd.com@mail.ru>
2023-12-26 16:44:36 +0300

    parsival_changes

--- src/test_lib_json/jsontest.cpp
+++ src/test_lib_json/jsontest.cpp
@@ -405,7 +405,9 @@ void Runner::printUsage(const char* appName) {
 // //////////////////////////////////////////////////////////////////
 
 Json::String ToJsonString(const char* toConvert) {
-  return Json::String(toConvert);
+  if (toConvert)
+    return Json::String(toConvert);
+  return "";
 }
 
 Json::String ToJsonString(Json::String in) { return in; }
